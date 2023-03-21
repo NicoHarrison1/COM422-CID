5cd2da47f0a3f231f2c1dba3d65f8a85d5aa70ff
Bazza-MacBook <bazzaware@gmail.com>
2023-03-21 10:44:49 +0000

    setting env for visual code

---
+++ tests/cartest.py
@@ -0,0 +1,13 @@
+def test_car() -> None:
+    """
+    Test for new car
+    """
+
+    # arrange
+    expected = True
+
+    # act
+    result = False
+
+    # assert
+    assert result == expected
