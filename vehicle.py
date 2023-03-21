226c2841e47cbf0c872de1cb1db97e0049d7d457
Nicola Harrison <6harrn42@solent.ac.uk>
2023-03-21 10:43:30 +0000

    Made base class

---
+++ vehicle.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+
+class Vehicle(ABC):
+
+    class Vehicle(ABC):
+        def __init__(self, registration, weight):
+            self.registration = registration
+            self.weight = weight
+    @abstractmethod
+    def Calculcate_Fee(selfself):
+        pass
+
